769c907535141df7a765db467ea66bae26cfefda
kartonas <jokubas.mickus@gmail.com>
2016-05-09 20:19:39 +0300

    working, cleaned up

--- char_task.rb
+++ char_task.rb
@@ -1,9 +1,9 @@
-require "json"
-file = File.open(ARGV[0],"r").read.gsub(" ", "")
-counter = file.split("").count
+require 'json'
+file = File.open(ARGV[0], 'r').read.gsub('' '', '')
+counter = file.split('').count
 hsh = Hash.new(0)
-file.split("").each do |letter|
-  hsh[letter] += 1.to_f/counter*100
+file.split('').each do |letter|
+  hsh[letter] += 1.to_f / counter * 100
 end
-a = hsh.sort_by {|k,w| w}.reverse
-File.new(ARGV[1],'w').puts a.to_json
+a = hsh.sort_by { |_k, w| w }.reverse
+File.new(ARGV[1], 'w').puts a.to_json
